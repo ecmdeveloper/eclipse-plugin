789f09e3a87c6c4d1fed22230e1c626cd5db0fb3
rickxx <rickxx@5485e650-ad50-4929-b1a6-0ee02d539c3b>
2009-09-20 06:21:16 +0000

    Connected handlers to eclipse menu's

--- com.ecmdeveloper.plugin/src/com/ecmdeveloper/plugin/handlers/DeleteObjectStoreItemHandler.java
+++ com.ecmdeveloper.plugin/src/com/ecmdeveloper/plugin/handlers/DeleteObjectStoreItemHandler.java
@@ -1,9 +1,22 @@
 package com.ecmdeveloper.plugin.handlers;
 
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.ecmdeveloper.plugin.handlers.RenameObjectStoreItemHandler.NameValidator;
+import com.ecmdeveloper.plugin.model.CodeModuleFile;
+import com.ecmdeveloper.plugin.model.CodeModulesManager;
+import com.ecmdeveloper.plugin.model.IObjectStoreItem;
 
 /**
  * 
@@ -14,7 +27,29 @@ public class DeleteObjectStoreItemHandler extends AbstractHandler implements IHa
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		if (window == null)	return null;
+
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+
+		if (!(selection instanceof IStructuredSelection))
+			return null;
+
+		Iterator<?> iterator = ((IStructuredSelection) selection).iterator();
+		while ( iterator.hasNext() ) {
+
+			Object elem = iterator.next();
+
+			if ( elem instanceof IObjectStoreItem )
+			{
+				String name = ((IObjectStoreItem)elem).getName();
+				boolean answerTrue = MessageDialog.openQuestion( window.getShell(), "Delete", "Do you want to delete '" +  name + "'?" );
+				if ( answerTrue )
+				{
+				}
+			}
+		}
 		return null;
 	}
 
