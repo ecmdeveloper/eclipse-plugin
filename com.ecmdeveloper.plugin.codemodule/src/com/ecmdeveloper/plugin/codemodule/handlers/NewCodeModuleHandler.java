411cbb7a40b638aabf522eaa42e55d9ec6b72838
Ricardo Belfor <rickx1@users.noreply.github.com>
2009-10-17 19:17:08 +0000

    Added support for new code module.

---
+++ com.ecmdeveloper.plugin.codemodule/src/com/ecmdeveloper/plugin/codemodule/handlers/NewCodeModuleHandler.java
@@ -0,0 +1,31 @@
+package com.ecmdeveloper.plugin.codemodule.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.ecmdeveloper.plugin.codemodule.wizard.NewCodeModuleWizard;
+
+public class NewCodeModuleHandler extends AbstractHandler implements IHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		if (window == null)	return null;
+
+		NewCodeModuleWizard wizard = new NewCodeModuleWizard();
+		
+		wizard.init( window.getWorkbench(), null );
+
+		WizardDialog dialog = new WizardDialog(window.getShell(), wizard);
+		dialog.create();
+		dialog.open();
+		
+		return null;
+	}
+}
--- com.ecmdeveloper.plugin.codemodule/src/com/ecmdeveloper/plugin/codemodule/wizard/NewCodeModuleWizardPage.java
+++ com.ecmdeveloper.plugin.codemodule/src/com/ecmdeveloper/plugin/codemodule/wizard/NewCodeModuleWizardPage.java
@@ -10,6 +10,8 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
@@ -17,23 +19,36 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
+import com.ecmdeveloper.plugin.codemodule.util.Messages;
 import com.ecmdeveloper.plugin.model.ObjectStore;
 import com.ecmdeveloper.plugin.views.ObjectStoreItemLabelProvider;
 
 public class NewCodeModuleWizardPage extends WizardPage {
 
+	private static final String PAGE_NAME = Messages.NewCodeModuleWizardPage_PageName;
+	private static final String DESCRIPTION = Messages.NewCodeModuleWizardPage_Description;
+	private static final String TITLE = Messages.NewCodeModuleWizardPage_Title;
+	private static final String CONNECT_LABEL = Messages.NewCodeModuleWizardPage_ConnectLabel;
+	private static final String SELECT_OBJECT_STORE_LABEL = Messages.NewCodeModuleWizardPage_SelectObjectStoreLabel;
+	private static final String NAME_LABEL = Messages.NewCodeModuleWizardPage_NameLabel;
+	
 	private Button connectButton;
 	private ComboViewer objectStoresCombo;
+	private NewCodeModuleWizard wizard;
+	private Text nameText;
 	
 	public NewCodeModuleWizardPage() {
-		super( "newCodeModule" );
-		setTitle("New Code Module");
-		setDescription("Select the new Code Module location." );
+		super( PAGE_NAME );
+		setTitle( TITLE );
+		setDescription(DESCRIPTION );
 	}
 
 	@Override
 	public void createControl(Composite parent) {
+
+		wizard = (NewCodeModuleWizard) getWizard();
 
 		Composite container = new Composite(parent, SWT.NULL );
 		final GridLayout gridLayout = new GridLayout();
@@ -42,11 +57,26 @@ public class NewCodeModuleWizardPage extends WizardPage {
 		setControl(container);
 		
 		Label selectObjectStoreLabel = new Label(container, SWT.NULL);
-		selectObjectStoreLabel.setText("Select Object Store:");
+		selectObjectStoreLabel.setText(SELECT_OBJECT_STORE_LABEL);
 
 		createObjectStoresCombo( container );
 		createConnectButton( container );
+		createNameText( container );
+	}
+
+	private void createNameText(Composite container) {
+		final Label nameLabel = new Label( container, SWT.NONE );
+		nameLabel.setLayoutData( new GridData(GridData.BEGINNING) );
+		nameLabel.setText(NAME_LABEL);
 		
+		nameText = new Text(container, SWT.BORDER);
+		nameText.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				wizard.setName( nameText.getText() );
+			}
+		});
+		nameText.setLayoutData( new GridData(GridData.FILL_HORIZONTAL) );
 	}
 
 	private void createObjectStoresCombo(Composite container ) {
@@ -66,14 +96,13 @@ public class NewCodeModuleWizardPage extends WizardPage {
 
 				Iterator<?> iterator = ((IStructuredSelection) selection).iterator();
 				if ( iterator.hasNext() ) {
-//					wizard.setObjectStore( (ObjectStore) iterator.next() );
-//					connectButton.setEnabled( ! wizard.getObjectStore().isConnected() );
-//					updateCodeModulesCombo( false );
+					wizard.setObjectStore( (ObjectStore) iterator.next() );
+					connectButton.setEnabled( ! wizard.getObjectStore().isConnected() );
 				}
 			} 
 		});
 		
-//		objectStoresCombo.setInput( wizard.getObjectStores() );
+		objectStoresCombo.setInput( wizard.getObjectStores() );
 		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
 		objectStoresCombo.getCombo().setLayoutData( gd );
 	}
@@ -81,12 +110,17 @@ public class NewCodeModuleWizardPage extends WizardPage {
 	private void createConnectButton( Composite parent ) {
 		
 		connectButton = new Button( parent, SWT.PUSH);
-		connectButton.setText( "Connect..." );
+		connectButton.setText( CONNECT_LABEL );
 		connectButton.setEnabled( false );
 		connectButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-//				updateCodeModulesCombo( true );
+
+				ObjectStore objectStore = wizard.getObjectStore();
+				if ( ! objectStore.isConnected() ) {
+					wizard.connectObjectStore();
+					connectButton.setEnabled( ! wizard.getObjectStore().isConnected() );
+				}
 			}
 		});
 
