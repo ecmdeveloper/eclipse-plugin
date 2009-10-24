b0040dc7277fe699aa774b416b9fc1910c179a33
rickxx <rickxx@5485e650-ad50-4929-b1a6-0ee02d539c3b>
2009-10-24 19:29:47 +0000

    Refactored for asynchonous actions support.

--- com.ecmdeveloper.plugin/src/com/ecmdeveloper/plugin/wizard/ImportObjectStoreWizard.java
+++ com.ecmdeveloper.plugin/src/com/ecmdeveloper/plugin/wizard/ImportObjectStoreWizard.java
@@ -1,7 +1,11 @@
 package com.ecmdeveloper.plugin.wizard;
 
+import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
 import java.util.Collection;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
@@ -10,9 +14,13 @@ import org.eclipse.ui.IWorkbench;
 
 import com.ecmdeveloper.plugin.model.ContentEngineConnection;
 import com.ecmdeveloper.plugin.model.ObjectStoresManager;
+import com.ecmdeveloper.plugin.util.PluginLog;
+import com.ecmdeveloper.plugin.util.PluginMessage;
 
 public class ImportObjectStoreWizard extends Wizard implements IImportWizard {
 	
+	private static final String CONNECT_MESSAGE = "Connecting to \"{0}\"";
+	private static final String CONNECT_TITLE = "Connect";
 	private SelectConnectionWizardPage selectConnectionWizardPage;
 	private ConfigureConnectionWizardPage configureConnectionWizardPage;
 	private SelectObjectStoreWizardPage selectObjectStoreWizardPage;
@@ -39,6 +47,7 @@ public class ImportObjectStoreWizard extends Wizard implements IImportWizard {
 		addPage(selectObjectStoreWizardPage);
 
 		setWindowTitle( "Import Object Store" );
+		setNeedsProgressMonitor(true);
 	}
 
 	@Override
@@ -88,29 +97,63 @@ public class ImportObjectStoreWizard extends Wizard implements IImportWizard {
 		getContainer().updateButtons();
 	}
 	
-	public void connect( ContentEngineConnection connection ) {
+	public void connect( final ContentEngineConnection connection ) {
 		
 		if ( ! connection.isConnected() ) {
-			objectStoresManager.connectConnection( connection.getName() );
+			
+	      try {
+	    	  getContainer().run(true, false, new IRunnableWithProgress() {
+	    		  public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+	    			  performConnect(connection, monitor);
+	    		  }
+	    	  });
+			} catch (InvocationTargetException e) {
+				PluginMessage.openError(getShell(), CONNECT_TITLE, e.getLocalizedMessage(), e );
+			} catch (InterruptedException e) {
+				// User canceled, so stop but don’t close wizard.
+			}
+			
+		} else {
+			connected = true;
+			connectionName = connection.getName();
+			getContainer().updateButtons();
 		}
-		connected = true;
-		connectionName = connection.getName();
-		getContainer().updateButtons();
 	}
 	
 	public void connect() {
 		
-		String url = configureConnectionWizardPage.getURL();
-		String username = configureConnectionWizardPage.getUsername();
-		String password = configureConnectionWizardPage.getPassword();
+		final String url = configureConnectionWizardPage.getURL();
+		final String username = configureConnectionWizardPage.getUsername();
+		final String password = configureConnectionWizardPage.getPassword();
 		
-		connectionName = objectStoresManager.createConnection( url, username, password );
-		connected = true;
-
-		getContainer().updateButtons();
+	      // Perform the operation in a separate thread
+	      // so that the operation can be canceled.
+		try {
+	         getContainer().run(true, false, new IRunnableWithProgress() {
+	            public void run(IProgressMonitor monitor) throws InvocationTargetException,
+	                  InterruptedException {
+	        		connectionName = objectStoresManager.createConnection( url, username, password, monitor );
+	        		connected = true;
+	        		getContainer().updateButtons();
+	            }
+	         });
+	      }
+	      catch (InvocationTargetException e) {
+				PluginMessage.openError(getShell(), CONNECT_TITLE, e.getLocalizedMessage(), e );
+	      }
+	      catch (InterruptedException e) {
+	    	  // Should not happen
+	      }
 	}
 	
 	public String[] getObjectStores() {
 		return objectStoresManager.getNewObjectstoreNames(connectionName);
+	}
+
+	private void performConnect(final ContentEngineConnection connection, IProgressMonitor monitor) {
+		objectStoresManager.connectConnection(connection.getName(), monitor );
+		connected = true;
+		connectionName = connection.getName();
+		getContainer().updateButtons();
 	}
 }
