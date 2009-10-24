f59a1534d47dfb0945890349b4cd9132a9813780
rickxx <rickxx@5485e650-ad50-4929-b1a6-0ee02d539c3b>
2009-10-24 19:28:33 +0000

    Added hourglass icon

--- com.ecmdeveloper.plugin/src/com/ecmdeveloper/plugin/util/IconFiles.java
+++ com.ecmdeveloper.plugin/src/com/ecmdeveloper/plugin/util/IconFiles.java
@@ -5,5 +5,6 @@ public class IconFiles {
 	public static final String ICON_OBJECTSTORE = "icons/database.png";
 	public static final String ICON_ACTION = "icons/script_lightning.png";
 	public static final String ICON_CODEMODULE = "icons/script.png";
+	public static final String HOURGLASS = "icons/hourglass.png";
 	
 }
