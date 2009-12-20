a96b98a303ec848122387da9bd1f148edd110c2d
Ricardo Belfor <rickx1@users.noreply.github.com>
2009-12-20 09:41:55 +0000

    Started with a folder editor.

--- com.ecmdeveloper.plugin/src/com/ecmdeveloper/plugin/util/IconFiles.java
+++ com.ecmdeveloper.plugin/src/com/ecmdeveloper/plugin/util/IconFiles.java
@@ -25,5 +25,5 @@ public class IconFiles {
 	public static final String ICON_ACTION = "icons/script_lightning.png";
 	public static final String ICON_CODEMODULE = "icons/script.png";
 	public static final String HOURGLASS = "icons/hourglass.png";
-	
+	public static final String FOLDER_EDIT = "icons/folder_edit.png";
 }
