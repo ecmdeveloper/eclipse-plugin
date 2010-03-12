53f4b2a1007439c80c29f5cc7f1f3dd499330b2b
rickxx <rickxx@5485e650-ad50-4929-b1a6-0ee02d539c3b>
2010-03-12 20:19:33 +0000

    Worked on attribute relationships.

---
+++ com.ecmdeveloper.plugin.classes/src/com/ecmdeveloper/plugin/classes/model/task/GetRequiredClassDescription.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2010, Ricardo Belfor
+ * 
+ * This file is part of the ECM Developer plug-in. The ECM Developer plug-in
+ * is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * The ECM Developer plug-in is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ECM Developer plug-in. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.ecmdeveloper.plugin.classes.model.task;
+
+import com.filenet.api.meta.PropertyDescriptionObject;
+
+/**
+ * @author Ricardo.Belfor
+ *
+ */
+public class GetRequiredClassDescription extends BaseTask {
+
+	private PropertyDescriptionObject propertyDescription;
+	
+	public GetRequiredClassDescription(PropertyDescriptionObject propertyDescription) {
+		this.propertyDescription = propertyDescription;
+	}
+
+	@Override
+	public Object call() throws Exception {
+		com.filenet.api.meta.ClassDescription requiredClass = propertyDescription.get_RequiredClass();
+//		ClassDescription classDescription = new ClassDescription(requiredClass, null, objectStore );
+//		return classDescription;
+		return requiredClass;
+	}
+
+}
