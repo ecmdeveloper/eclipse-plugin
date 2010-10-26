77d76baefe93b81c8bc33e9fb0a0452a327e7600
Ricardo Belfor <rickx1@users.noreply.github.com>
2010-10-26 12:52:54 +0000

    Improve delete function

---
+++ com.ecmdeveloper.plugin.ui/src/com/ecmdeveloper/plugin/ui/perspectives/ContentEnginePerspectiveFactory.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2010, Ricardo Belfor
+ * 
+ * This file is part of the ECM Developer plug-in. The ECM Developer plug-in
+ * is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * The ECM Developer plug-in is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ECM Developer plug-in. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.ecmdeveloper.plugin.ui.perspectives;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.progress.IProgressConstants;
+
+/**
+ * @author ricardo.belfor
+ *
+ */
+public class ContentEnginePerspectiveFactory implements IPerspectiveFactory {
+
+	private static final String CLASS_DIAGRAM_PERSPECTIVE_ID = "com.ecmdeveloper.plugin.diagrams.classDiagramPerspective";
+	
+	private static final String TRACKED_FILES_VIEW_ID = "com.ecmdeveloper.plugin.views.TrackedFiles";
+	private static final String CODE_MODULES_VIEW_ID = "com.ecmdeveloper.plugin.views.codeModulesView";
+	private static final String OBJECT_STORE_FAVORITES_VIEW_ID = "com.ecmdeveloper.plugin.views.ObjectStoreFavorites";
+	private static final String OBJECT_STORES_VIEW_ID = "com.ecmdeveloper.plugin.views.ObjectStoresView";
+	private static final String CLASSES_VIEW_ID = "com.ecmdeveloper.plugin.classes.views.ClassesView";
+	
+	private static final String ID_PACKAGES = "org.eclipse.jdt.ui.PackageExplorer";
+	private static final String ID_PROJECT_EXPLORER = "org.eclipse.ui.navigator.ProjectExplorer";
+
+	private static final String NEW_CLASS_DIAGRAM_COMMAND_ID = "com.ecmdeveloper.plugin.diagrams.wizards.NewClassDiagramWizard";
+	private static final String NEW_CODE_MODULE_COMMAND_ID = "com.ecmdeveloper.plugin.wizard.newCodeModule";
+	private static final String NEW_FOLDER_COMMAND_ID = "com.ecmdeveloper.plugin.newFolder";
+	private static final String NEW_DOCUMENT_COMMAND_ID = "com.ecmdeveloper.plugin.newDocument";
+	private static final String NEW_CUSTOM_OBJECT_COMMAND_ID = "com.ecmdeveloper.plugin.newCustomObject";
+	private static final String NEW_EVENT_ACTION_COMMAND_ID = "com.ecmdeveloper.plugin.wizard.newEventAction";
+	private static final String NEW_DOCUMENT_CLASSIFIER_COMMAND_ID = "com.ecmdeveloper.plugin.wizard.newDocumentClassifier";
+	private static final String NEW_DOCUMENT_LIFECYCLE_ACTION_HANDLER_COMMAND_ID = "com.ecmdeveloper.plugin.wizard.newDocumentLifecycleActionHandler";
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+
+		String editorArea = layout.getEditorArea();
+		
+		addLeftViews(layout, editorArea);
+		addRightViews(layout, editorArea);
+		addBottomViews(layout, editorArea);
+		addShortcuts(layout);
+	}
+
+	private void addBottomViews(IPageLayout layout, String editorArea) {
+		
+		IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.66f, editorArea );
+		
+		bottom.addView(CODE_MODULES_VIEW_ID );
+		bottom.addView(TRACKED_FILES_VIEW_ID);
+		
+		bottom.addPlaceholder(IPageLayout.ID_PROP_SHEET);
+		bottom.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
+		bottom.addPlaceholder(IPageLayout.ID_PROP_SHEET);
+	}
+
+	private void addRightViews(IPageLayout layout, String editorArea) {
+		IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, 0.66f, editorArea );
+		right.addView( IPageLayout.ID_RES_NAV );
+		right.addPlaceholder(ID_PACKAGES);
+		right.addPlaceholder(ID_PROJECT_EXPLORER);
+	}
+
+	private void addLeftViews(IPageLayout layout, String editorArea) {
+		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.25f, editorArea );
+		left.addView( OBJECT_STORES_VIEW_ID);
+		left.addView( OBJECT_STORE_FAVORITES_VIEW_ID );
+		left.addPlaceholder( CLASSES_VIEW_ID );
+	}
+
+	private void addShortcuts(IPageLayout layout) {
+		addPerspectiveShortcuts(layout);
+		addShowViewShortcuts(layout);
+		addNewWizardShortcuts(layout);
+	}
+
+	private void addPerspectiveShortcuts(IPageLayout layout) {
+		layout.addPerspectiveShortcut(CLASS_DIAGRAM_PERSPECTIVE_ID);
+	}
+
+	private void addShowViewShortcuts(IPageLayout layout) {
+		layout.addShowViewShortcut(OBJECT_STORES_VIEW_ID);
+		layout.addShowViewShortcut(OBJECT_STORE_FAVORITES_VIEW_ID);
+		layout.addShowViewShortcut(CODE_MODULES_VIEW_ID );
+		layout.addShowViewShortcut(TRACKED_FILES_VIEW_ID);
+		layout.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
+		layout.addShowViewShortcut(CLASSES_VIEW_ID);
+	}
+
+	private void addNewWizardShortcuts(IPageLayout layout) {
+		layout.addNewWizardShortcut(NEW_CLASS_DIAGRAM_COMMAND_ID);
+		layout.addNewWizardShortcut(NEW_CODE_MODULE_COMMAND_ID);
+		layout.addNewWizardShortcut(NEW_FOLDER_COMMAND_ID);
+		layout.addNewWizardShortcut(NEW_DOCUMENT_COMMAND_ID);
+		layout.addNewWizardShortcut(NEW_CUSTOM_OBJECT_COMMAND_ID);
+		layout.addNewWizardShortcut(NEW_EVENT_ACTION_COMMAND_ID);
+		layout.addNewWizardShortcut(NEW_DOCUMENT_LIFECYCLE_ACTION_HANDLER_COMMAND_ID);
+		layout.addNewWizardShortcut(NEW_DOCUMENT_CLASSIFIER_COMMAND_ID);
+	}
+}
---
+++ com.ecmdeveloper.plugin.ui/src/com/ecmdeveloper/plugin/ui/wizard/DeleteWizard.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright 2010, Ricardo Belfor
+ * 
+ * This file is part of the ECM Developer plug-in. The ECM Developer plug-in
+ * is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * The ECM Developer plug-in is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ECM Developer plug-in. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.ecmdeveloper.plugin.ui.wizard;
+
+import java.util.Collection;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.wizard.Wizard;
+
+import com.ecmdeveloper.plugin.jobs.DeleteJob;
+import com.ecmdeveloper.plugin.model.Folder;
+import com.ecmdeveloper.plugin.model.IObjectStoreItem;
+
+/**
+ * @author ricardo.belfor
+ *
+ */
+public class DeleteWizard extends Wizard {
+
+	private static final String WINDOW_TITLE = "Delete";
+	private static final String DELETE_MESSAGE = "Do you want to perform the delete operation?";
+	
+	private PreviewDeleteWizardPage confirmDeleteWizardPage;
+	private ConfigureDeleteWizardPage configureDeleteWizardPage;
+	
+	private Collection<IObjectStoreItem> itemsDeleted;
+	
+	public DeleteWizard(Collection<IObjectStoreItem> itemsDeleted) {
+		setWindowTitle(WINDOW_TITLE);
+		this.itemsDeleted = itemsDeleted;
+	}
+
+	@Override
+	public void addPages() {
+		
+		if ( isFolderDeleted() ) {
+			configureDeleteWizardPage = new ConfigureDeleteWizardPage();
+			addPage( configureDeleteWizardPage );
+		}
+		confirmDeleteWizardPage = new PreviewDeleteWizardPage();
+		confirmDeleteWizardPage.setItemsDeleted(itemsDeleted);
+		addPage(confirmDeleteWizardPage);
+	}
+
+	private boolean isFolderDeleted() {
+		for ( IObjectStoreItem objectStoreItem : itemsDeleted ) {
+			if ( objectStoreItem instanceof Folder) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isDeleteContainedDocuments() {
+		return configureDeleteWizardPage != null
+				&& configureDeleteWizardPage.isDeleteContainedDocuments();
+	}
+
+	public boolean isDeleteContainedCustomObjects() {
+		return configureDeleteWizardPage != null
+				&& configureDeleteWizardPage.isDeleteContainedCustomObjects();
+	}
+
+	public boolean isDeleteContainedFolders() {
+		return configureDeleteWizardPage != null
+				&& configureDeleteWizardPage.isDeleteContainedFolders();
+	}
+
+	@Override
+	public boolean performFinish() {
+
+		boolean answerTrue = MessageDialog.openQuestion(getShell(), getWindowTitle(), DELETE_MESSAGE );
+		if (answerTrue) {
+			DeleteJob deleteJob = createDeleteJob();
+			deleteJob.setUser(true);
+			deleteJob.schedule();
+			return true;
+		}
+		return false;
+	}
+
+	private DeleteJob createDeleteJob() {
+		DeleteJob deleteJob = new DeleteJob(itemsDeleted, getShell() );
+		deleteJob.setDeleteContainedDocuments(isDeleteContainedDocuments() );
+		deleteJob.setDeleteContainedCustomObjects(isDeleteContainedCustomObjects() );
+		deleteJob.setDeleteContainedFolders(isDeleteContainedFolders() );
+		return deleteJob;
+	}
+}
