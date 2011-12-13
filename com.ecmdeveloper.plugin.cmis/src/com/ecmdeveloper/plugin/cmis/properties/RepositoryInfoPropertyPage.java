1e1914f676be95a488ecd3b505f245fdb9d0b5ba
rickxx <rickxx@5485e650-ad50-4929-b1a6-0ee02d539c3b>
2011-12-13 12:48:56 +0000

    Refactored property pages

---
+++ com.ecmdeveloper.plugin.cmis/src/com/ecmdeveloper/plugin/cmis/properties/RepositoryInfoPropertyPage.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2011, Ricardo Belfor
+ * 
+ * This file is part of the ECM Developer plug-in. The ECM Developer plug-in
+ * is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * The ECM Developer plug-in is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ECM Developer plug-in. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.ecmdeveloper.plugin.cmis.properties;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+import com.ecmdeveloper.plugin.cmis.model.ObjectStore;
+
+/**
+ * @author ricardo.belfor
+ *
+ */
+public class RepositoryInfoPropertyPage  extends PropertyPage implements IWorkbenchPropertyPage {
+
+	@Override
+	protected Control createContents(Composite parent) {
+		
+		ObjectStore objectStore = (ObjectStore) getElement();
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
